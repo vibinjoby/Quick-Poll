20d7cd3d1813ae467f332660184889d60b070ec8
vibin <vibin2joby@gmail.com>
2020-06-09 12:32:37 -0400

    login signup changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+  document.getElementById("loginContainer").onclick = onLoginClick;
+  document.getElementById("logoContainer").onclick = onLogoClick;
   gsap.from(".pollBtn", { duration: 1, z: -200, opacity: 0, scale: 0.5 });
   gsap.from("#content", { duration: 1, z: -200, opacity: 0, scale: 0.5 });
   gsap.from("#logoLarge", {
@@ -7,4 +9,51 @@ window.onload = function() {
     opacity: 0,
     scale: 0.5
   });
+};
+
+const onLogoClick = () => {
+  window.location.href = "/";
+};
+
+const onLoginClick = () => {
+  let tl = gsap.timeline({ paused: false });
+  gsap.to("#contentContainer", 1, { y: 200, autoAlpha: 0 });
+  gsap.from("#signinContainer", 1, {
+    y: -200,
+    duration: 2,
+    autoAlpha: 1
+  });
+  document.getElementById("signinContainer").style.display = "block";
+};
+
+const onSignin = () => {
+  document.getElementById("signup-tab").style.display = "none";
+  gsap.from("#signin-tab", 1, {
+    duration: 0.5,
+    z: -200,
+    opacity: 0,
+    scale: 0.5
+  });
+  document.getElementById("signin-tab").style.display = "block";
+
+  if (document.querySelector(".nav-item a.active") !== null) {
+    document.querySelector(".nav-item a.active").classList.remove("active");
+  }
+  document.getElementById("login").classList.add("active");
+};
+
+const onSignUp = () => {
+  document.getElementById("signin-tab").style.display = "none";
+  gsap.from("#signup-tab", 1, {
+    duration: 0.2,
+    z: -200,
+    opacity: 0,
+    scale: 0.5
+  });
+  document.getElementById("signup-tab").style.display = "block";
+
+  if (document.querySelector(".nav-item a.active") !== null) {
+    document.querySelector(".nav-item a.active").classList.remove("active");
+  }
+  document.getElementById("signup").classList.add("active");
 };
